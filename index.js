aa09a2e967ff841e1cb999abb57d342a7684c2ac
RnbWd <dwisner6@gmail.com>
2015-05-07 17:19:26 -0700

    update

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-var task = require('local-dympl');
+var path = require('path');
 
-task('default', task.once('push'));
-
-task('add', function(t) {
-  t.exec('git add .').then(t.done);
-});
-
-task('commit', task.once('add'), function(t) {
-  if (!t.params.msg) t.params.msg = 'update';
-  t.exec('git commit -m "{msg}"', t.params).then(t.done);
-});
-
-task('push', task.once('commit'), function(t) {
-  t.exec("git push").then(t.done);
-});
+module.exports = path.join(__dirname, 'bin/git-update');
