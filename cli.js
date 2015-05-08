9363782a40227c2ab70426b14d44ef32a800de58
RnbWd <dwisner6@gmail.com>
2015-05-07 17:43:56 -0700

    update

--- cli.js
+++ cli.js
@@ -3,6 +3,6 @@
 var spawn = require('child_process').spawn;
 var input = process.argv.slice(2);
 var cmd = require('./').concat(input);
-console.log(cmd);
+
 spawn('bash', cmd, {stdio: 'inherit'})
   .on('exit', process.exit);
